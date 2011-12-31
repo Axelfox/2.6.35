06d82192a25e8c8e85f406a7ec72011e72890e1f
Mike Gapinski <mikegapinski@gmail.com>
2011-12-31 00:32:32 -0800

    Update to beta 3 with all patches except the incr one

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 600 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#600 PREEMPT Fri Dec 30 12:37:32 PST 2011"
+#define LINUX_COMPILE_TIME "12:37:32"
+#define LINUX_COMPILE_BY "mikegapinski"
+#define LINUX_COMPILE_HOST "MacBookPro6_1"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
